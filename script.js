3b2a4f4f7d4bf8977e2b793220a21dde9137cf63
CIRUS.mike.note <pinskiy.miahil@gmail.com>
2022-08-08 14:22:51 +0300

    1st version

---
+++ script.js
@@ -0,0 +1,23 @@
+const signInBtn  = document.querySelector('.signin-btn');
+const signInBtng  = document.querySelector('.signing-btn');
+const formBox = document.querySelector('.form-box');
+const body = document.querySelector('.block');
+const buttonColor = document.querySelector('.block-item__btn')
+
+
+
+signInBtng.addEventListener('click', function() {
+    formBox.classList.add('active'); 
+    body.classList.add('active');
+    buttonColor.classList.add('active');
+   
+    
+});
+
+signInBtn.addEventListener('click', function() {
+    formBox.classList.remove('active');
+    body.classList.remove('active');
+    buttonColor.classList.remove('active');
+    
+    
+});
